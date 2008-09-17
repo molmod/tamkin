c59759198125a4c34c05dd32e759ddae19e2e845
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2008-09-17 13:16:11 +0200

    Added license conditions: GPL v3

--- test/test.py
+++ test/test.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
+# TAMkin is a post-processing toolkit for thermochemistry and kinetics analysis.
+# Copyright (C) 2008 Toon Verstraelen <Toon.Verstraelen@UGent.be>, 
+# Matthias Vandichel <Matthias.Vandichel@UGent.be> and
+# An Ghysels <An.Ghysels@UGent.be>
+#
+# This file is part of TAMkin.
+#
+# TAMkin is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# TAMkin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
 
 import sys, unittest
 

e95f29e7b43e6960df8bc5765e99b251add3d106
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-04-29 10:52:48 +0200

    Small fixes in the scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # This is a very simplistic install scipt. Use with care!
 
 if [ -n $1 ] && [ "$1" = "--system" ]; then
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 # This is a very simplistic uninstall scipt. Use with care!
 
 if [ -n $1 ] && [ "$1" = "--system" ]; then
   rm -vr /usr/lib/python*/site-packages/tamkin
 else
-  if [ -z $PYTHONPATH ]
+  if [ -z $PYTHONPATH ]; then
     echo 'WARNING: $PYTHONPATH is not defined, defaulting to \$HOME/lib/python'
     PYTHONPATH=$HOME/lib/python
   fi

558918188e06650e666e798749d4ea9e112d1baa
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-05-15 09:26:55 +0200

    Updated license with mandatory references

--- test/tools.py
+++ test/tools.py
@@ -1,7 +1,8 @@
 # TAMkin is a post-processing toolkit for thermochemistry and kinetics analysis.
 # Copyright (C) 2008-2009 Toon Verstraelen <Toon.Verstraelen@UGent.be>,
 # Matthias Vandichel <Matthias.Vandichel@UGent.be> and
-# An Ghysels <An.Ghysels@UGent.be>
+# An Ghysels <An.Ghysels@UGent.be>, Center for Molecular Modeling (CMM), Ghent
+# University, Ghent, Belgium; all rights reserved unless otherwise stated.
 #
 # This file is part of TAMkin.
 #
@@ -9,6 +10,40 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
+#
+# In addition to the regulations of the GNU General Public License,
+# publications and communications based in parts on this program or on
+# parts of this program are required to cite the following five articles:
+#
+# "Vibrational Modes in partially optimized molecular systems.", An Ghysels,
+# Dimitri Van Neck, Veronique Van Speybroeck, Toon Verstraelen and Michel
+# Waroquier, Journal of Chemical Physics, Vol. 126 (22): Art. No. 224102, 2007
+# DOI:10.1063/1.2737444
+#
+# "Cartesian formulation of the Mobile Block Hesian Approach to vibrational
+# analysis in partially optimized systems", An Ghysels, Dimitri Van Neck and
+# Michel Waroquier, Journal of Chemical Physics, Vol. 127 (16), Art. No. 164108,
+# 2007
+# DOI:10.1063/1.2789429
+#
+# "Calculating reaction rates with partial Hessians: validation of the MBH
+# approach", An Ghysels, Veronique Van Speybroeck, Toon Verstraelen, Dimitri Van
+# Neck and Michel Waroquier, Journal of Chemical Theory and Computation, Vol. 4
+# (4), 614-625, 2008
+# DOI:10.1021/ct7002836
+#
+# "Mobile Block Hessian approach with linked blocks: an efficient approach for
+# the calculation of frequencies in macromolecules", An Ghysels, Veronique Van
+# Speybroeck, Ewald Pauwels, Dimitri Van Neck, Bernard R. Brooks and Michel
+# Waroquier, Journal of Chemical Theory and Computation, Vol. 5 (5), 1203-1215,
+# 2009
+# DOI:10.1021/ct800489r
+#
+# "Normal modes for large molecules with arbitrary link constraints in the
+# mobile block Hessian approach", An Ghysels, Dimitri Van Neck, Bernard R.
+# Brooks, Veronique Van Speybroeck and Michel Waroquier, Journal of Chemical
+# Physics, Vol. 130 (18), Art. No. 084107, 2009
+# DOI:10.1063/1.3071261
 #
 # TAMkin is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of

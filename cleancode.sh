d928486026d6338472b1f3c2f614e1ac01772693
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-03-22 16:44:14 +0100

    Also clean rst files

--- cleancode.sh
+++ cleancode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Cleaning python code in $(pwd) and subdirectories"
-for file in `find lib test examples | egrep "(\.py$)|(\.f90$)|(^scripts/tr-)"`; do
+for file in `find doc lib test examples | egrep "(\.py$)|(\.rst$)|(\.f90$)|(^scripts/tr-)"`; do
   echo Cleaning ${file}
   sed -i -e $'s/\t/    /' ${file}
   sed -i -e $'s/[ \t]\+$//' ${file}

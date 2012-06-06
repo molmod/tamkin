56426d234e46e1418e9c6dfa79872cd734b9d63c
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-06-06 15:25:26 +0200

    Remove dumb scripts

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is a very simplistic uninstall scipt. Use with care!
-
-if [ -n $1 ] && [ "$1" = "--system" ]; then
-  rm -vr /usr/local/lib/python*/site-packages/tamkin
-else
-  rm -vr $HOME/lib/python/tamkin
-fi

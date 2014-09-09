fb13e1917ef8b01a786c42e4c8e80fbd294d34f6
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2014-09-09 12:52:14 +0200

    Release 1.0.5

--- tamkin/__init__.py
+++ tamkin/__init__.py
@@ -34,7 +34,7 @@
 #--
 """TAMkin is a post-processing toolkit for normal mode, thermochemistry and kinetics."""
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 from tamkin.chemmod import *
 from tamkin.data import *

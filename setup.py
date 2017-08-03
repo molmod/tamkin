b2ad9f0d57e1fbaaa4605e23bb48c47b770c4362
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-08-03 15:54:46 +0200

    Increment dependency version

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(name='TAMkin',
     packages = ['tamkin', 'tamkin.io'],
     scripts=["scripts/tamkin-driver.py"],
     install_requires=['numpy>=1.0', 'nose>=0.11', 'matplotlib>1.1',
-                      'molmod>=1.3.1', 'scipy>=0.17.1'],
+                      'molmod>=1.3.2', 'scipy>=0.17.1'],
     classifiers=[
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',

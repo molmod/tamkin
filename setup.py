b87859948914b05472e683df06093ceb454cf93b
Toon Verstraelen <toon.verstraelen@UGent.be>
2019-09-10 08:43:07 +0200

    Few improvements in CI configuration for py37

--- setup.py
+++ setup.py
@@ -102,8 +102,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Physics',
